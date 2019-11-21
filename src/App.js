8a7065610e3a39d2e1258d8a5f6986780af2a909
Tobias <tobi.l@posteo.at>
2019-11-21 11:38:59 +0100

    feat: Add Article

--- src/App.js
+++ src/App.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Article from "./components/article"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <Article
+          headline="Awsome feature toggles and how they work"
+          text="Feature toogle are awesome to change the behavier of your app without changing your code" />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default Home
---
+++ src/components/article.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Article = ({ headline, text }) => (
+  <div>
+    <h2>{headline}</h2>
+    <p>{text}</p>
+  </div>
+)
+
+export default Article
